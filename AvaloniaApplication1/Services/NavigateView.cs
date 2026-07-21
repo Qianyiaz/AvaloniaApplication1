b909cf838f546949bb9abdf11304adf7fab3f7e6
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2026-07-21 15:07:52 +0800

    Move window animation to XAML; fix NavigateView
    Refactor startup animation out of MainWindow code-behind into XAML styles and transitions. Adds a Window style with initial opacity/transform and visible-state transitions, updates the Loaded trigger to use AsyncActionGroup Mode="Sequence" and a 1s delay. Simplifies NavigateView.Navigate to an expression-bodied switch that sets Target.Content via the IServiceProvider and removes the old OnLoaded animation implementation and unused usings.

--- AvaloniaApplication1/Services/NavigateView.cs
+++ AvaloniaApplication1/Services/NavigateView.cs
@@ -5,15 +5,13 @@ namespace AvaloniaApplication1.Services;
 public class NavigateView(IServiceProvider sp) : INavigateView
 {
     public ContentControl Target { get; set; } = null!;
-    public void Navigate(int id)
+
+    public void Navigate(int id) => Target.Content = id switch
     {
-        Target.Content = id switch
-        {
-            1 => sp.GetService(typeof (HomePage)),
-            2 => sp.GetService(typeof (SettingsPage)),
-            _ => throw new ArgumentOutOfRangeException(nameof(id), id, null)
-        };
-    }
+        1 => sp.GetService(typeof(HomePage)),
+        2 => sp.GetService(typeof(SettingsPage)),
+        _ => throw new ArgumentOutOfRangeException(nameof(id), id, null)
+    };
 }
 
 public interface INavigateView
--- AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -1,77 +1,8 @@
-using Avalonia;
-using Avalonia.Animation;
-using Avalonia.Animation.Easings;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Media;
-using Avalonia.Styling;
 
 namespace AvaloniaApplication1;
 
 public partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
-
-    protected override async void OnLoaded(RoutedEventArgs e)
-    {
-        base.OnLoaded(e);
-
-        RenderTransformOrigin = new RelativePoint(0.5, 0.5, RelativeUnit.Relative);
-
-        var transformGroup = new TransformGroup();
-        var translate = new TranslateTransform(100, 0);
-        var scale = new ScaleTransform(0.85, 0.85);
-        transformGroup.Children.Add(translate);
-        transformGroup.Children.Add(scale);
-        RenderTransform = transformGroup;
-        Opacity = 0;
-
-        await Task.WhenAll(new Animation
-        {
-            Duration = TimeSpan.FromSeconds(0.45),
-            FillMode = FillMode.Forward,
-            Children =
-            {
-                new KeyFrame { Cue = new Cue(0d), Setters = { new Setter(OpacityProperty, 0d) } },
-                new KeyFrame { Cue = new Cue(1d), Setters = { new Setter(OpacityProperty, 1d) } }
-            },
-            Easing = new SineEaseOut()
-        }.RunAsync(this), new Animation
-        {
-            Duration = TimeSpan.FromSeconds(0.6),
-            FillMode = FillMode.Forward,
-            Children =
-            {
-                new KeyFrame { Cue = new Cue(0d), Setters = { new Setter(TranslateTransform.XProperty, 50d) } },
-                new KeyFrame { Cue = new Cue(1d), Setters = { new Setter(TranslateTransform.XProperty, 0d) } }
-            },
-            Easing = new BackEaseOut()
-        }.RunAsync(this), new Animation
-        {
-            Duration = TimeSpan.FromSeconds(0.6),
-            FillMode = FillMode.Forward,
-            Children =
-            {
-                new KeyFrame
-                {
-                    Cue = new Cue(0d),
-                    Setters =
-                    {
-                        new Setter(ScaleTransform.ScaleXProperty, 0.85),
-                        new Setter(ScaleTransform.ScaleYProperty, 0.85)
-                    }
-                },
-                new KeyFrame
-                {
-                    Cue = new Cue(1d),
-                    Setters =
-                    {
-                        new Setter(ScaleTransform.ScaleXProperty, 1d),
-                        new Setter(ScaleTransform.ScaleYProperty, 1d)
-                    }
-                }
-            },
-            Easing = new BackEaseOut()
-        }.RunAsync(this));
-    }
 }
